93a571fd6500dd1c4ec669c8fc2d40d2e006ac04
Zhenzhong Tang <zztang@seas.upenn.edu>
2023-12-11 04:49:18 +0000

    test

--- benchmarks/benchmark_serving_first_token_latency.sh
+++ benchmarks/benchmark_serving_first_token_latency.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+echo "------------------"
+echo "     Network      "
+echo "------------------"
+for i in {1..10}
+do
+    # dummy bad requests
+    curl -w "@curl-format.txt" -o /dev/null http://localhost:8000/v1/chat/completions \
+        -H "Content-Type: application/json" \
+        -d '{
+            "model": "facebook/opt-6.7b",
+            "messages": [
+                {"role": "user", "content": "Tell me about CIS 565 at UPenn."}
+            ],
+            "max_tokens": 1,
+        }'
+done
+
+echo "------------------"
+echo "     No cache     "
+echo "------------------"
 curl -w "@curl-format.txt" -o /dev/null http://localhost:8000/v1/chat/completions \
     -H "Content-Type: application/json" \
     -d '{
-        "model": "facebook/opt-125m",
+        "model": "facebook/opt-6.7b",
+        "messages": [
+            {"role": "user", "content": "Tell me about CIS 565 at UPenn."}
+        ],
+        "max_tokens": 1
+    }'
+
+echo "------------------"
+echo "     With cache   "
+echo "------------------"
+curl -w "@curl-format.txt" -o /dev/null http://localhost:8000/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -d '{
+        "model": "facebook/opt-6.7b",
         "messages": [
             {"role": "user", "content": "Tell me about CIS 565 at UPenn."}
         ],
